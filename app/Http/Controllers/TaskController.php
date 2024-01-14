e7d88c00d6c26b156978615a03bb29aaaf8372f1
gshendra <gshendra@gmail.com>
2024-01-15 00:05:21 +1100

    Develop Edit Task View

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -66,4 +66,14 @@ class TaskController extends Controller
         ]);
     }
 
+    public function edit($id)
+    {
+        $pageTitle = 'Edit Task';
+        $tasks = $this->tasks;
+
+        $task = $tasks[$id - 1];
+
+        return view('tasks.edit', ['pageTitle' => $pageTitle, 'task' => $task]);
+    }
+
 }
